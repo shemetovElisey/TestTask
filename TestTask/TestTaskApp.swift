315e8643794d3ff621e5ea4178318f1b3190adc4
Elisey <shemetov.elisey@gmail.com>
2021-02-06 01:51:59 +0700

    Initial Commit

---
+++ TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Елисей on 06.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
